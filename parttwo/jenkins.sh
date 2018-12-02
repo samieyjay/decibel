5d2d4bfb7165f23962dee155a78ad9d978bb45ff
samieyjay <38917093+samieyjay@users.noreply.github.com>
2018-12-02 20:16:57 +0000

    Create jenkins.sh

---
+++ parttwo/jenkins.sh
@@ -0,0 +1,14 @@
+# Add the repository key to the system
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+
+
+sudo apt-get update
+
+# Install Jenkins and its dependencies
+sudo apt-get install jenkins
+
+# Start Jenkins
+sudo systemctl start jenkins
+
+# Open port 8080 for Jenkins
+sudo ufw allow 8080
