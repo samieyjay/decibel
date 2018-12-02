5d331a6cc301e6b6aef7ba69ac8175962180365c
samieyjay <38917093+samieyjay@users.noreply.github.com>
2018-12-02 20:42:28 +0000

    Update backup.sh

--- partone/backup.sh
+++ partone/backup.sh
@@ -11,5 +11,5 @@ touch rsync-dir/file{1..20}
 #start sync between server 1 and 2
 rsync -avrt --delete -e 'ssh -p 22' /root/rsync-dir/ root@172.31.24.202:~/downloads
 
-# setup cronjob to scan and backup new files every 15mins
+# setup cronjob to scan and backup new files every 15mins /etc/crontab or crontab -e
 # 15 * * * * root rsync -avrt --delete -e 'ssh -p 22' /root/rsync-dir/ root@172.31.24.202:~/downloads
